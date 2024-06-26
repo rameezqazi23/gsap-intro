acd134d2295f96e33bdbbf5e8b3301330665949a
rameezqazi23 <rameez.qazi23@gmail.com>
2024-06-26 17:04:51 +0500

    Animate grid boxes with stagger animation with different stagger properties.

--- src/pages/GsapStagger.jsx
+++ src/pages/GsapStagger.jsx
@@ -1,5 +1,25 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapStagger = () => {
   // TODO: Implement the gsap.stagger() method
+
+  useGSAP(() => {
+    gsap.to(".stagger-box", {
+      y: 250,
+      yoyo: true,
+      repeat: -1,
+      ease: "sine.in",
+      // stagger: 0.5,
+      stagger: {
+        amount: 0.5,
+        grid: [4, 1],
+        axis: "y",
+        ease: "circ.inOut",
+        from: "center",
+      },
+    });
+  }, []);
 
   return (
     <main>
